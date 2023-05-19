fecf3f94ab71721e8a80aeb4062f6c5b343a1c01
Pathompong Muangthong <p.s3xyy@gmail.com>
2023-05-20 04:26:14 +0700

    Update construct.py

--- GraphPrepKit/construct.py
+++ GraphPrepKit/construct.py
@@ -1,1 +1,10 @@
+def node_indice(dfConn, source: str, target: str):
+    # Construct nodes indices
+    all_topic_ids = dfConn[source].unique().tolist()
+    all_content_ids = dfConn[target].unique().tolist()
 
+    topic_to_idx = dict(zip(all_topic_ids, range(len(all_topic_ids))))
+    content_to_idx = dict(zip(all_content_ids, range(len(all_content_ids))))
+
+    idx_to_topic = {v: k for k, v in topic_to_idx.items()}
+    idx_to_content = {v: k for k, v in content_to_idx.items()}
