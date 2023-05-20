4608a5f088393dcd0862c71913c8d5654949db83
Pathompong Muangthong <p.s3xyy@gmail.com>
2023-05-20 13:33:20 +0700

    Update hetero.py

--- GraphPrepKit/hetero.py
+++ GraphPrepKit/hetero.py
@@ -1,4 +1,11 @@
-def edge_index(dfConn, source: str, source: str):
+import numpy as np
+
+def edge_index_numpy(dfConn, source: str, target: str):
+    # Construct `edge_index` of self-loop and undirected graph
+    edge_index = np.transpose(dfConn[[source, target]].to_numpy())
+    return edge_index
+
+def edge_index_torch(dfConn, source: str, source: str):
     # Construct `edge_index` of non-self-loop and directed graph
     edge_index = torch.tensor(np.transpose(dfConn[[source, source]].to_numpy()),dtype=torch.long)
     return edge_index
