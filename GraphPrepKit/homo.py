73ab3882a0e7ab7b669c45bbec1ffd2b34d487c1
Pathompong Muangthong <p.s3xyy@gmail.com>
2023-05-21 08:51:31 +0700

    Update homo.py

--- GraphPrepKit/homo.py
+++ GraphPrepKit/homo.py
@@ -1,18 +1,1 @@
-import numpy as np
 
-def edge_index_numpy(dfConn, source: str, target: str):
-    # Construct `edge_index` of self-loop and undirected graph
-    edge_index = np.transpose(dfConn[[source, target]].to_numpy())
-    return edge_index
-
-def edge_index_torch(dfConn, source: str, target: str):
-    # Construct `edge_index` of self-loop and undirected graph
-    edge_index = torch.tensor(np.transpose(contentGraph[[source, target]].to_numpy()),dtype=torch.long)
-    return edge_index
-
-def get_mutual_table(dfConn, on: str, by: str, self_loop=True):
-    # Construct homogenous graph (sigle node type) with undirected edge
-    contentGraph = dfConn.merge(dfConn, on=by)
-    if not self_loop:
-        contentGraph = contentGraph.loc[contentGraph[on + '_x'] != contentGraph[on + '_y']]
-    return contentGraph
